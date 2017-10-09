44f0ea923ffcfbdeb788226e1198194c5ab13db6
GabrielGobbi <contato@conceitoart.com.br>
2017-10-09 14:36:38 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\user;
 
 $app = new Slim();
 
@@ -21,12 +23,61 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 	
 
 });
+
+$app->get('/admin_login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("login");
+	
+
+});
+
+$app->get('/admin/login', function() {
+    
+ 	/*User::login($_POST["login"], $_POST["password"]);*/
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("login");
+
+	/*header("location: /admin");
+	exit;*/
+	
+
+});
+
+$app->post('/admin/login', function() {
+    
+	User::login($_POST["login"], $_POST["password"]);
+
+	/*$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("login");*/
+
+	header("location: /admin");
+	exit;
+	
+
+});
+
 
 $app->run();
 
